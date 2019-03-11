b058751bad7eba3db2cee3a3aa6ed12b70d4ce10
rangheet <heetdave@outlook.com>
2019-03-11 10:19:33 +0530

    Removed cypress.

--- cypress/integration/app_spec.js
+++
@@ -1,7 +0,0 @@
-describe("Knowledge Base Application", () => {
-    it("Shows a text", () => {
-      cy.visit("/");
-      cy.get('#root').get(".MainComponent").get("h1:first")
-        .should("have.text", "Hello, Sherlock!");
-    });
-  });
