e9560eb815edb10dd2679e00ba6abba38a2d2a46
rangheet <heetdave@outlook.com>
2019-03-30 20:40:57 +0530

    Monor changes

--- webpack.config.js
+++ webpack.config.js
@@ -20,14 +20,14 @@ module.exports = {
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
-    path: path.resolve(__dirname, "build/"),
-    publicPath: "/build/",
+    path: path.resolve(__dirname, "dist/"),
+    publicPath: "/dist/",
     filename: "bundle.js"
   },
   devServer: {
     contentBase: path.join(__dirname, "public/"),
     port: 8080,
-    publicPath: "http://localhost:8080/build/",
+    publicPath: "http://localhost:8080/dist/",
     hotOnly: true
   },
   plugins: [new webpack.HotModuleReplacementPlugin()]
